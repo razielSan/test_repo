81241228144c38192c8bc98c7540d6ebcb3f174e
raz <test@gmail.com>
2023-05-13 14:27:27 +0700

    Full commit

--- test_palindrom.py
+++ test_palindrom.py
@@ -1,4 +1,9 @@
-def test_palindrom(input_value, expected):
+def test_palindrom(input_value: str, expected: str):
+    """
+        Вычисляет явлеяется ли строка полиндромом
+    """
+
+    age = 100
+
     input_value = input_value.lower().replace(' ', '')
     return input_value == input_value[::-1]
-
