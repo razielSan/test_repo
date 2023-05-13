7cd123712f2a7a9928790a884d329ac9c5a00af2
raz <test@gmail.com>
2023-05-13 15:29:04 +0700

    'age'

--- test_palindrom.py
+++ test_palindrom.py
@@ -3,7 +3,8 @@ def test_palindrom(input_value: str, expected: str):
         Вычисляет явлеяется ли строка полиндромом
     """
 
-    name = 1
+    name = "Lena"
+    age = 100
 
     input_value = input_value.lower().replace(' ', '')
     return input_value == input_value[::-1]
