d51570f175c6f096e7fec217dbbdcd22c593adbb
raz <test@gmail.com>
2023-05-13 14:47:58 +0700

    'name'

--- test_palindrom.py
+++ test_palindrom.py
@@ -2,5 +2,8 @@ def test_palindrom(input_value: str, expected: str):
     """
         Вычисляет явлеяется ли строка полиндромом
     """
+
+    name = 1
+
     input_value = input_value.lower().replace(' ', '')
     return input_value == input_value[::-1]
