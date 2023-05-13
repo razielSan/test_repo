42651f1a23e2990a5208c9cc98bc99d711d0706e
raz <test@gmail.com>
2023-05-13 14:27:27 +0700

    'commit_2'

--- test_palindrom.py
+++ test_palindrom.py
@@ -1,4 +1,6 @@
-def test_palindrom(input_value, expected):
+def test_palindrom(input_value: str, expected: str):
+    """
+        Вычисляет явлеяется ли строка полиндромом
+    """
     input_value = input_value.lower().replace(' ', '')
     return input_value == input_value[::-1]
-
